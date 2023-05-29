4a85204bca54c5b577b5fec20c34f39f8d7533ea
Your name hereKonstantin <your_email@example.comkonstantinschiller@gmail.com>
2023-05-29 08:33:36 -0400

    Added functionality to search a term and fetch the result from the db

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const global = {
-    currentPage: window.location.pathname
+    currentPage: window.location.pathname,
+    search: {
+        term: '',
+        type: '',
+        page: 1,
+        totalPages: 1
+    },
+    //do not do this on live products - just for testing in local environment
+    api: {
+        apiKey: '542ff4ce8f90a24e70473fae77e0dc7c',
+        apiUrl: 'https://api.themoviedb.org/3'
+    }
 };
 
 async function displayPopularMovies() {
@@ -219,6 +230,24 @@ function displayBackgroundImage(type, imagePath){
     }
 }
 
+// Search Movie and Shows
+
+async function search () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    global.search.type = urlParams.get('type');
+    global.search.term = urlParams.get('search-term');
+
+    if (global.search.term !== '' && global.search.term !== null) {
+        const results = await searchAPIData();
+        console.log(results);
+    } else {
+       showAlert('Please enter a search term');
+    }
+
+}
+
 // Display Slider
 
 async function displaySlider() {
@@ -277,12 +306,33 @@ function initSwiper(){
 
 async function fetchAPIData(endpoint){
     // Register your key at themoviedb.org/settings/api and enter here. Only use this for development or very small projects. You should store your key and make requests from a server.
-    const API_KEY = '542ff4ce8f90a24e70473fae77e0dc7c';
-    const API_URL = 'https://api.themoviedb.org/3';
+    /* const API_KEY = '542ff4ce8f90a24e70473fae77e0dc7c';
+    const API_URL = 'https://api.themoviedb.org/3'; */
+
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl; 
 
     showSpinner();
 
     const response = await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data = await response.json();
+
+    hideSpinner();
+
+    return data;
+}
+
+// Make request to search
+
+async function searchAPIData(){
+
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl; 
+
+    showSpinner();
+
+    const response = await fetch(`${API_URL}/search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
 
     const data = await response.json();
 
@@ -310,6 +360,18 @@ function highlightActiveLink (){
     })
 }
 
+// Show Alert
+
+function showAlert(message, className) {
+    const alertEl = document.createElement('div');
+    alertEl.classList.add('alert', className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+
+    setTimeout(() => alertEl.remove(), 3000);
+}
+
+
 
 function addCommasToNumber(number){
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -336,7 +398,7 @@ function init() {
             displayShowDetails();
             break;
         case '/search.html':
-            console.log('Search');
+            search();
             break;
     }
 
